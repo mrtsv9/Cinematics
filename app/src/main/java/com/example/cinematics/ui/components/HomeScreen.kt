8c56d312f64b2a25920570d1f66ec57b1bfd5608
MartsevPC <a.martsev85@gmail.com>
2022-12-13 01:03:42 +0300

    some changes

--- app/src/main/java/com/example/cinematics/ui/components/HomeScreen.kt
+++ app/src/main/java/com/example/cinematics/ui/components/HomeScreen.kt
@@ -3,38 +3,51 @@ package com.example.cinematics.ui.components
 import android.app.Activity
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.paging.PagingData
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
+import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
 import com.example.cinematics.data.datasource.remote.ApiURL
 import com.example.cinematics.data.model.MovieItem
 import com.example.cinematics.navigation.NavigationScreen
 import com.example.cinematics.navigation.currentRoute
 import com.example.cinematics.utils.PagingLoadingState
 import com.example.cinematics.utils.items
+import com.example.cinematics.R
+import com.example.cinematics.ui.theme.DividerColor
+import com.example.cinematics.ui.theme.LightBlack
+import com.example.cinematics.ui.theme.White
 import kotlinx.coroutines.flow.Flow
 
 @Composable
 fun HomeScreen(
     navController: NavController,
-    movies: Flow<PagingData<MovieItem>>
+    movies: Flow<PagingData<MovieItem>>,
 ) {
     val activity = (LocalContext.current as? Activity)
     val progressBar = remember {
@@ -51,17 +64,45 @@ fun HomeScreen(
     Column {
         Column {
             CircularIndeterminateProgressBar(isDisplayed = progressBar.value, verticalBias = 0.4f)
-            LazyVerticalGrid(
-                columns = GridCells.Fixed(2),
-                modifier = Modifier.padding(start = 5.dp, top = 5.dp, end = 5.dp),
-                content = {
-                    items(moviesItem) { item ->
-                        item?.let {
-                            MovieItemView(item, navController)
+            LazyColumn {
+                items(moviesItem.itemCount) { counter ->
+                    val item = moviesItem[counter]
+                    Row(modifier = Modifier
+                        .height(180.dp)
+                        .fillMaxWidth()
+                        .background(White)
+                        .padding(horizontal = 12.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Start) {
+
+                        ImageItem(title = item?.title,
+                            modifier = Modifier
+                                .height(160.dp)
+                                .wrapContentWidth(),
+                            url = item?.posterPath)
+
+                        Spacer(modifier = Modifier.width(12.dp))
+                        Column(verticalArrangement = Arrangement.SpaceEvenly,
+                            horizontalAlignment = Alignment.Start) {
+                            Text(text = item?.title.toString(),
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold)
+                            Text(text = item?.releaseDate.toString(), fontSize = 20.sp)
+                            Row(modifier = Modifier.wrapContentSize(),
+                                verticalAlignment = Alignment.CenterVertically) {
+                                Image(painterResource(id = R.drawable.ic_star_16),
+                                    contentDescription = null,
+                                    modifier = Modifier.wrapContentSize())
+                                Spacer(modifier = Modifier.width(4.dp))
+                                Text(text = item?.voteAverage.toString(),
+                                    fontSize = 20.sp,
+                                    color = Color.Black)
+                            }
                         }
                     }
+                    Divider(color = Color.Black, thickness = 1.dp)
                 }
-            )
+            }
         }
         if (openDialog.value) {
             ExitAlertDialog(navController, {
@@ -76,6 +117,18 @@ fun HomeScreen(
     }
 }
 
+@OptIn(ExperimentalCoilApi::class)
+@Composable
+fun ImageItem(
+    url: String?,
+    modifier: Modifier = Modifier,
+    title: String? = "Title",
+) {
+    Image(painter = rememberAsyncImagePainter(ApiURL.IMAGE_URL.plus(url)),
+        contentDescription = title,
+        modifier = modifier)
+}
+
 @Composable
 fun MovieItemView(item: MovieItem, navController: NavController) {
     Column(modifier = Modifier.padding(7.5.dp)) {
@@ -87,7 +140,6 @@ fun MovieItemView(item: MovieItem, navController: NavController) {
                 .clip(MaterialTheme.shapes.large)
                 .clickable {
                     navController.navigate(NavigationScreen.MovieDetail.MOVIE_DETAIL.plus("/${item.id}"))
-                }
-        )
+                })
     }
 }
--- app/src/main/java/com/example/cinematics/ui/theme/Color.kt
+++ app/src/main/java/com/example/cinematics/ui/theme/Color.kt
@@ -9,3 +9,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+val LightBlack = Color(0xFF1D1D1D)
+val White = Color.White
+val DividerColor = Color(0xFF313131)
